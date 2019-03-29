b40769eafc706df77e98f1efb33b5b95140d3f27
Aryan Sharma <asharma00@stuy.edu>
2019-03-29 09:20:47 -0400

    add and construcotr

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,41 @@
+
+
 public class MyLinkedList {
+  private int size;
+  private Node start,end;
+
   public String toString()
-  
-public MyLinkedList<E>()
-    //construct an empty list
-public void clear()
-    //reset the list to an empty state. Very similar to the constructor.
-public boolean/void add(E)
-    //add an element to the end of the list (the boolean would be true all the time if you want to conform to list standards)
+
+public MyLinkedList<E>() {   //construct an empty list
+  size = 0;
+  start = null;
+  end = null;
+}
+
+public void clear() { //reset the list to an empty state. Very similar to the constructor.
+  size = 0;
+  start = null;
+  end = null;
+}
+
+public boolean add(E) {
+  if (size == 0) {
+    Node x = new Node(value, null, null);
+    start = x;
+    end = x;
+    size++;
+    return true;
+  }
+  else {
+    Node x = new Node(value, null, end);
+    end.setNext(x);
+    end = x;
+    size++;
+    return true;
+  }
+
+}//add an element to the end of the list (the boolean would be true all the time if you want to conform to list standards)
+
 public void extend(MyLinkedList<E> other)
   //   in O(1) time, connect the other list to the end of this list.
   //  The other list is then reset to size 0 (do not wipe out the nodes, just disconnect them.)
